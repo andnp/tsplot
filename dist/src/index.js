a345498ad383d38e53fbe4613a3fa2324bfec303
andy <andy.patterson@genesys.com>
2017-11-14 19:35:36 -0500

    build files

--- dist/src/index.js
+++ dist/src/index.js
@@ -3,6 +3,10 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 Object.defineProperty(exports, "__esModule", { value: true });
+const Bluebird = require("bluebird");
+Bluebird.config({
+    cancellation: true
+});
 var Matrix_1 = require("./utils/Matrix");
 exports.Matrix = Matrix_1.default;
 __export(require("./utils/MatrixUtils"));
--- dist/src/plotters/RemotePlotter.js
+++ dist/src/plotters/RemotePlotter.js
@@ -2,9 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const SocketIO = require("socket.io");
 const Bluebird = require("bluebird");
+const _ = require("lodash");
+const getIO = _.memoize(SocketIO);
 class RemotePlotter {
-    connect(port = 80) {
-        const io = SocketIO(port);
+    connect(port = 2222) {
+        const io = getIO(port);
         this.connectionPromise = new Bluebird((resolve) => {
             io.of('/tsplot')
                 .once('connection', (socket) => {
@@ -13,8 +15,24 @@ class RemotePlotter {
         });
     }
     sendData(data) {
-        return this.connectionPromise.then(({ socket }) => {
+        return this.connectionPromise.tap(({ socket }) => {
             socket.emit('data', data);
+        });
+    }
+    on(event, func) {
+        this.connectionPromise.tap(({ socket }) => {
+            console.log('registered');
+            socket.on(event, func);
+        });
+    }
+    onDisconnect() {
+        return this.connectionPromise.then(({ socket }) => {
+            return new Bluebird((resolve) => {
+                socket.on('disconnect', () => {
+                    console.log('disconnected');
+                    resolve();
+                });
+            });
         });
     }
 }
