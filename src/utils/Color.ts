739e31ec13af20f3b45e70d2fdab1f347768cf9d
andy <andnpatterson@gmail.com>
2019-02-08 22:14:17 -0700

    add diverging color palette

--- src/utils/Color.ts
+++ src/utils/Color.ts
@@ -123,3 +123,21 @@ export const createSequentialPalette = (colors: number = 9) => {
 
     return new Palette(rawPalette.slice(0, colors));
 }
+
+export const createDivergingPalette = (colors: number = 1) => {
+    const rawPalette = [
+        new Color([84, 48, 5]),
+        new Color([140, 81, 10]),
+        new Color([191, 129, 45]),
+        new Color([223, 194, 125]),
+        new Color([246, 232, 195]),
+        new Color([245, 245, 245]),
+        new Color([199, 234, 229]),
+        new Color([128, 205, 193]),
+        new Color([53, 151, 143]),
+        new Color([1, 102, 94]),
+        new Color([0, 60, 48]),
+    ];
+
+    return new Palette(rawPalette.slice(0, colors));
+}
